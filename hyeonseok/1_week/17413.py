dd60d69417a374c4a0525541dc492eefe0eff5c5
bbbbooo <105257665+bbbbooo@users.noreply.github.com>
2023-09-09 18:25:45 +0900

    백준 / 17413 / python

--- hyeonseok/1_week/17413.py
+++ hyeonseok/1_week/17413.py
@@ -1,16 +1,26 @@
-s = input().split()
-stack = []
-
+import sys
+s = sys.stdin.readline().strip() + ' '
+stack = [] # 스택
+result = '' # 결과물 출력
+cnt = 0 # 괄호 여부
 
 for i in s:
-    if i[-1] in '<':
-        print(i)
-    if i[-1] in '>':
-        print(i)
-    else:
-        rs = i[::-1]
-        stack.append(rs)
+    if i == '<':
+        cnt = 1
+        for _ in range(len(stack)):
+            result += stack.pop()
+    stack.append(i)
 
+    if i == '>':
+        cnt  = 0
 
-# print(stack)
-print(' '.join(stack))
+        for _ in range(len(stack)):
+            result += stack.pop(0)
+
+    if i == ' ' and cnt == 0:
+        stack.pop()
+        for _ in range(len(stack)):
+            result += stack.pop()
+        result += ' '
+
+print(result)
